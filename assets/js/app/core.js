2bb902950ba03bf2063e95819993375d9118db10
Vladimir Kuznetsov <vladimir.kuznetsov@engine6.com>
2013-09-10 09:14:28 +0600

    log sync arguments

--- assets/js/app/core.js
+++ assets/js/app/core.js
@@ -1,1 +1,12 @@
 var App = {};
+
+(function () {
+
+	var sync = Backbone.sync;
+
+	Backbone.sync = function (method) {
+		console.log("sync: ", arguments);
+		sync.apply(this, arguments);
+	};
+
+}());
